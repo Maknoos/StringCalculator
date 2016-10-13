dd49bcdf983b96dcf8b1b04befc42f9ff6a9aa20
Magnus Gunnarsson <magnusgun15@ru.is>
2016-10-13 17:17:54 +0000

    some simple refactoring

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,23 +5,25 @@ public class Calculator {
 		if(input == ""){
 			return 0;
 		}
+
 		if(input.contains(",")){
 			String [] numbers = input.split(",");
 			int arraySize = numbers.length;
 			int counter = 0;
-			int stringSum =0;
+			int stringSum = 0;
+
 			while(counter!=arraySize){
 				int temp=stringToInt(numbers[counter]);
 				stringSum += temp;
 				counter ++;
 			}
+
 			return stringSum;
 		}
 
 		return stringToInt(input);
 	}
-	private static int stringToInt(String input)
-	{
+	private static int stringToInt(String input){
 		return Integer.parseInt(input);
 	}
 }
