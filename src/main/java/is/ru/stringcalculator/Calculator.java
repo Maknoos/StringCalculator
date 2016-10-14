05991d7500a523f762578689063c973db5f97736
Magnus Gunnarsson <magnusgun15@ru.is>
2016-10-14 15:36:17 +0000

    added exception support for multiple negative arguments

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -10,15 +10,33 @@ public class Calculator {
 		if(input.contains(",")||input.contains("\n")){
 			String [] numbers = input.split(splitExpression);
 			int arraySize = numbers.length;
+			int [] negatives = new int[arraySize];
 			int counter = 0;
 			int stringSum = 0;
+			boolean illegalInput = false;
 
 			while(counter!=arraySize){
 				int temp=stringToInt(numbers[counter]);
+				
+				if(temp < 0){
+					illegalInput = true;
+					negatives[counter] = temp;
+				}
+
 				stringSum += temp;
 				counter ++;
 			}
 
+			if(illegalInput){
+				String exceptionString = "Negatives not allowed:";
+				 for(int i =0; i< negatives.length; i++){
+					if(negatives[i]!=0)
+					{
+						exceptionString += negatives[i];
+					}
+				}
+				throw new IllegalArgumentException(exceptionString);
+			}
 			return stringSum;
 		}
 
@@ -32,14 +50,6 @@ public class Calculator {
 
 	private static int stringToInt(String input){
 		return Integer.parseInt(input);
-	}
-
-	private static void checkNegative(String input){
-		int temp = stringToInt(input);
-		if(temp<0){
-			String exceptionString = "Negatives not allowed:" + temp;
-			throw new IllegalArgumentException(exceptionString);
-		}
 	}
 
 	private static String splitExpression = "[\n,]"; 
